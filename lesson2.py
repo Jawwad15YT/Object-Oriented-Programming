a0556558ac2459544bb47c27757caa4202243771
Jawwad15YT <53828972+Jawwad15YT@users.noreply.github.com>
2024-10-24 22:38:05 +0600

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+class animal:
+    legs = True
+    eyes = True
+    kidneys = 2
+    hair = True
+    face = True
+    nose = True
+    
+
+
+
+tiger = animal()
+human = animal()
+rat = animal()
+monkey = animal()
+
+print(tiger.legs)
+print(human.eyes)
+print(human.kidneys)
+print(rat.hair)
+print(monkey.face)
+print(tiger.nose)
