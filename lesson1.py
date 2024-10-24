f1a12f3d2dfd97fde99d069762a5fe28d886edc7
Jawwad15YT <53828972+Jawwad15YT@users.noreply.github.com>
2024-10-24 22:28:30 +0600

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+class plant:
+    colour = "green"
+    leaves = True
+    photosynthesis = True
+    Flowers = True
+
+Mango_Tree = plant()
+print(Mango_Tree.colour)
+
+Apple_Tree = plant()
+print(Apple_Tree.Flowers)
+
+Banana_Tree = plant()
+print(Banana_Tree.colour)
+
+Coconut_Tree = plant()
+print(Coconut_Tree.leaves)
+
+grass = plant()
+print(grass.Flowers)
+
+seaweed = (plant)
+print(seaweed.photosynthesis)
+
+Jackfruit_tree = plant()
+print(Jackfruit_tree.colour)
